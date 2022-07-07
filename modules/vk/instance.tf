169bd6026b9020c047ffb93e730f073be95544e8
Alexey Kotov <atercat@gmail.com>
2022-07-07 19:47:12 +0300

    Revert "Multi-instance feature"
    This reverts commit e3671a9a809047ed22389948b7358bcc09c2c38c.

--- modules/vk/instance.tf
+++ modules/vk/instance.tf
@@ -1,18 +1,17 @@
 resource "vkcs_blockstorage_volume" "volume" {
-  name              = "${var.name}-storage${var.suffix}"
+  name              = "${var.name}-storage"
   size              = var.storage_size
   image_id          = var.image_id
   volume_type       = "ssd"
   availability_zone = "MS1"
 }
 resource "vkcs_compute_instance" "vm" {
-  name            = "${var.name}${var.suffix}"
+  name            = var.name
   image_id        = var.image_id
   flavor_name     = var.flavor_name
   key_pair        = var.key_pair
   security_groups = ["default","all"]
-  config_drive    = true
-  count           = var.vm_count
+  config_drive = true
 
   block_device {
     uuid                  = vkcs_blockstorage_volume.volume.id
